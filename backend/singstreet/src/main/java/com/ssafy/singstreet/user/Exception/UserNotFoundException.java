7db9f066150ebacf3275ed388a418a5a5f63085a
LeeSW-k <swl9455@naver.com>
2023-07-26 16:45:20 +0900

    Chore: User관련 파일 제거

--- backend/singstreet/src/main/java/com/ssafy/singstreet/user/Exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.singstreet.user.Exception;
-
-public class UserNotFoundException extends Exception{
-    public UserNotFoundException(String email){
-        System.out.println(email+"은 등록된 이메일이 아닙니다.");
-    }
-}
