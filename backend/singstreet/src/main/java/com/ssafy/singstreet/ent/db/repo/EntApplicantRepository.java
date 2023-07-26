313d45e78d97df0f47d66d5d24d427352af1c255
LeeSW-k <swl9455@naver.com>
2023-07-26 12:28:15 +0900

    Feat : Ent관련 Repository생성

---
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/repo/EntApplicantRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.singstreet.ent.db.repo;
+
+import com.ssafy.singstreet.ent.db.entity.EntApplicant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntApplicantRepository extends JpaRepository<EntApplicant, Integer> {
+}
---
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/repo/EntTagRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.singstreet.ent.db.repo;
+
+import com.ssafy.singstreet.ent.db.entity.EntTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntTagRepository extends JpaRepository<EntTag, Integer> {
+}
