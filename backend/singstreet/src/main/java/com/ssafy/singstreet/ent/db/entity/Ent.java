77241dd0a5e1fb40876bae73be69abff6f63a258
LeeSW-k <swl9455@naver.com>
2023-07-28 09:53:32 +0900

    Feat : Ent 삭제 기능 추가

--- backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/entity/Ent.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/entity/Ent.java
@@ -42,8 +42,8 @@ public class Ent extends BaseTimeEntity {
     @Column(name = "ent_img", length = 255)
     private String entImg;
 
-//    @Column(name = "created_at", nullable = false)
-//    private LocalDateTime createdAt;
+    @Column(name = "is_deleted", columnDefinition = "BOOLEAN DEFAULT false")
+    private boolean isDeleted;
 
     @Builder
     public Ent(User user, String entName, Boolean isAutoAccepted, String entInfo, String entImg){
@@ -59,6 +59,10 @@ public class Ent extends BaseTimeEntity {
         this.isAutoAccepted = isAutoAccepted;
         this.entInfo = entInfo;
         this.entImg = entImg;
+    }
+
+    public void delete(){
+        this.isDeleted = true;
     }
 
 }
--- backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/repo/EntRepository.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/repo/EntRepository.java
@@ -1,8 +1,13 @@
 package com.ssafy.singstreet.ent.db.repo;
 
 import com.ssafy.singstreet.ent.db.entity.Ent;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EntRepository extends JpaRepository<Ent, Integer> {
     Ent findByEntId(int entId);
+
+    Slice<Ent> findByIsDeleted(Boolean isDeleted, Pageable pageable);
+
 }
