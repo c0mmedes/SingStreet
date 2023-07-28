a2c4c59b25e50b7cee82ebcabc6968daff459840
LeeSW-k <swl9455@naver.com>
2023-07-29 01:36:54 +0900

    Fix : 자동 생성시간 삽입 불가 수정

--- backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/entity/EntMember.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/ent/db/entity/EntMember.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor // 기본 생성자
 @Entity
 @Table(name = "ent_member")
+@EntityListeners(AuditingEntityListener.class)
 public class EntMember {
 
     @EmbeddedId
