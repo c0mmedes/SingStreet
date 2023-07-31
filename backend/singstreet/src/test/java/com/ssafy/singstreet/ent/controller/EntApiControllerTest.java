01f47c69092977b5d4baa7d7af6e3f5d83973b8f
이준혁 <human646581@gmail.com>
2023-07-31 16:28:10 +0900

    Fix EntApiControllerTest 임시 비활성화

--- backend/singstreet/src/test/java/com/ssafy/singstreet/ent/controller/EntApiControllerTest.java
+++ backend/singstreet/src/test/java/com/ssafy/singstreet/ent/controller/EntApiControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.junit.jupiter.api.Disabled;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
