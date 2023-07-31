ee487b29e653e44f7099de0ae66544193dcffd8d
LeeSW-k <swl9455@naver.com>
2023-07-31 23:50:10 +0900

    Style : EntFeedCreateDto로 이름 변경하여 생성

--- backend/singstreet/src/main/java/com/ssafy/singstreet/ent/model/entFeedDto/EntFeedCreateRequestDto.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/ent/model/entFeedDto/EntFeedCreateRequestDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class EntFeedSaveRequestDto {
+public class EntFeedCreateRequestDto {
     private int user;
     private int ent;
     private String title;
