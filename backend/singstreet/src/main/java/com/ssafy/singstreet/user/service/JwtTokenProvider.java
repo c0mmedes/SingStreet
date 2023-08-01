fda04831f5c7236edb045a3c65f6fbd5763a90be
human646581@gmail.com <human646581@gmail.com>
2023-08-01 10:16:53 +0900

    Fix EntApiControllerTest 비활성화 && 임시 로그인 코드

--- backend/singstreet/src/main/java/com/ssafy/singstreet/user/service/JwtTokenProvider.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/user/service/JwtTokenProvider.java
@@ -32,7 +32,7 @@ public class JwtTokenProvider {
     }
 
     // 유저 정보를 가지고 AccessToken, RefreshToken 을 생성하는 메서드
-    public TokenInfo generateToken(Authentication authentication) {
+    public TokenInfo generateToken(org.springframework.security.core.Authentication authentication) {
         // 권한 가져오기
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
