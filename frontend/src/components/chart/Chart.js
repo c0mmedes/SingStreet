6bede28b1bbcf818279088b4bb91e637a4f15d61
nevermindperil <123xxx@kakao.com>
2023-08-03 02:09:06 +0900

    feat: chart 페이지 레이아웃

--- frontend/src/components/chart/Chart.js
+++ frontend/src/components/chart/Chart.js
@@ -1,9 +1,204 @@
 import React from "react";
 import Footer from "../layout/Footer";
+import "../../css/chart/Chart.css";
 const chart = () => {
   return (
     <div>
-      차트 게시판ASD
+      <div className="chartCard">
+        <div className="chartTitle">
+          <h1>HOT 10</h1>
+          <p>
+            The world liked a lot of music this month.
+            <br />
+            Discover the ranking.
+          </p>
+        </div>
+
+        <div className="chartBox">
+          <div className="chartNumber">01</div>
+          <div className="chartCover">
+            <img
+              src="https://m.media-amazon.com/images/I/91-kmdlsEsL._SS500_.jpg"
+              alt="01"
+            />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Mi gente</span> J Balin & Willy William
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=wnJ6LuUFpMo">Listen</a>
+          </div>
+        </div>
+
+        {/* <!-- Separator --> */}
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #02 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">02</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlCeWf" alt="02" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Feels</span> Calvin Harris
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=ozv4q2ov3Mk">Listen</a>
+          </div>
+        </div>
+
+        {/* <!-- Separator --> */}
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #03 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">03</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlRum1" alt="03" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Attention</span> Charlie Puth
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=nfs8NYg7yQM">Listen</a>
+          </div>
+        </div>
+
+        {/* <!-- Separator --> */}
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #03 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">03</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlRum1" alt="03" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Attention</span> Charlie Puth
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=nfs8NYg7yQM">Listen</a>
+          </div>
+        </div>
+
+        {/* <!-- Separator --> */}
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #03 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">03</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlRum1" alt="03" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Attention</span> Charlie Puth
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=nfs8NYg7yQM">Listen</a>
+          </div>
+        </div>
+
+        {/* <!-- Separator --> */}
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #03 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">03</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlRum1" alt="03" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Attention</span> Charlie Puth
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=nfs8NYg7yQM">Listen</a>
+          </div>
+        </div>
+
+        {/* <!-- Separator --> */}
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #03 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">03</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlRum1" alt="03" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Attention</span> Charlie Puth
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=nfs8NYg7yQM">Listen</a>
+          </div>
+        </div>
+
+        {/* <!-- Separator --> */}
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #03 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">03</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlRum1" alt="03" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Attention</span> Charlie Puth
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=nfs8NYg7yQM">Listen</a>
+          </div>
+        </div>
+
+        <div className="separator"></div>
+
+        <div className="chartBox">
+          {/* <!-- #03 --> */}
+          {/* <!-- Number--> */}
+          <div className="chartNumber">03</div>
+          {/* <!-- Cover --> */}
+          <div className="chartCover">
+            <img src="http://bit.ly/2vlRum1" alt="03" />
+          </div>
+          {/* <!-- Name --> */}
+          <div className="chartName">
+            <span>Attention</span> Charlie Puth
+          </div>
+          {/* <!-- Button --> */}
+          <div className="link">
+            <a href="https://www.youtube.com/watch?v=nfs8NYg7yQM">Listen</a>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
