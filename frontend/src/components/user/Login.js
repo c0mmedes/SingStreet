50b06d2d8d23de18c3b35cdd00b7f0d928fded6f
강현곤 <shoot_1@naver.com>
2023-08-02 16:37:59 +0900

    Feat: Login 테스트를 위한 push

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -1,69 +1,53 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../css/user/Login.css";
 import Background from "../layout/Background.js";
+import { api } from "../../services/httpService";
 
-// import Register from "./Register";
-function Login() {
-  const [inputId, setInputId] = useState("");
+const apiInstance = api();
+
+function Login({setUser}) {
+  const [inputEmail, setInputEmail] = useState("");
   const [inputPw, setInputPw] = useState("");
 
-  const handleInputId = (e) => {
-    setInputId(e.target.value);
+  const handleInputEmail = (e) => {
+    setInputEmail(e.target.value);
   };
 
   const handleInputPw = (e) => {
     setInputPw(e.target.value);
   };
 
-  const onClickLogin = () => {
+  const onClickLogin = async function login () {
     console.log("click login");
-    console.log("ID : ", inputId);
+    console.log("Email : ", inputEmail);
     console.log("PW : ", inputPw);
-    axios
-      .post("/user_inform/onLogin", null, {
-        params: {
-          user_id: inputId,
-          user_pw: inputPw,
-        },
-      })
-      .then((res) => {
+    
+    await apiInstance.post("/auth/login", JSON.stringify({
+      user_id: inputEmail,
+      user_pw: inputPw,
+    }))
+    .then((res) => {
         console.log(res);
         console.log("res.data.userId :: ", res.data.userId);
         console.log("res.data.msg :: ", res.data.msg);
-        if (res.data.userId === undefined) {
-          // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
-          console.log("======================", res.data.msg);
-          alert("입력하신 id 가 일치하지 않습니다.");
-        } else if (res.data.userId === null) {
-          // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
-          console.log(
-            "======================",
-            "입력하신 비밀번호 가 일치하지 않습니다."
-          );
-          alert("입력하신 비밀번호 가 일치하지 않습니다.");
-        } else if (res.data.userId === inputId) {
-          // id, pw 모두 일치 userId = userId1, msg = undefined
-          console.log("======================", "로그인 성공");
-          sessionStorage.setItem("user_id", inputId);
-        }
-        // 작업 완료 되면 페이지 이동(새로고침)
-        document.location.href = "/";
+        alert("로그인 성공");
       })
-      .catch();
+      .catch(() => {
+        alert("로그인 실패");
+      });
   };
 
   return (
     <div className="LoginWrap">
       <h1>LOGIN</h1>
       <div className="inputbox">
-        <label htmlFor="input_id">Email</label>
+        <label htmlFor="input_email">Email</label>
         <input
           type="text"
-          name="input_id"
-          value={inputId}
-          onChange={handleInputId}
+          name="input_email"
+          value={inputEmail}
+          onChange={handleInputEmail}
           className="IdInput"
         />
       </div>
