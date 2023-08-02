0ee0524511a826e4f318781083cb57dc386034db
강현곤 <shoot_1@naver.com>
2023-08-02 16:48:03 +0900

    axios baseURL 수정

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function api(){
   const instance = axios.create({
-    baseURL: 'https://api.example.com/', // API 서버의 기본 URL
+    baseURL: 'http://i9b110.p.ssafy.io:4050//', // API 서버의 기본 URL
     timeout: 5000, // 요청 시간 초과 설정 (옵션)
     headers: {
       'Content-Type': 'application/json',
