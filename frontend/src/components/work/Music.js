8323fc18490015b97f6b97cde9838828ce045848
nevermindperil <123xxx@kakao.com>
2023-08-04 00:02:04 +0900

    작품 페이지 기본 레이아웃 1차

--- frontend/src/components/work/Music.js
+++ frontend/src/components/work/Music.js
@@ -1,9 +1,118 @@
-import React from 'react';
+import React from "react";
+import "../../css/work/Music.css";
 
 const music = () => {
   return (
     <div>
-      작품 게시판
+      <form className="entSearchWrap">
+        <input
+          placeholder="노래합작 검색. . ."
+          type="text"
+          className="entSearchInput"></input>
+        <input type="submit" value="검색" className="entSearchSubmit"></input>
+        <select>
+          <option value="allEnt">최신순</option>
+          <option value="myEnt">인기순</option>
+        </select>
+      </form>
+      <div class="cards-list">
+        <div class="musicCard 1">
+          <div class="card_image">
+            <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
+          </div>
+          <div class="card_title title-white">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 2">
+          <div class="card_image">
+            <img src="https://cdn.blackmilkclothing.com/media/wysiwyg/Wallpapers/PhoneWallpapers_FloralCoral.jpg" />
+          </div>
+          <div class="card_title title-white">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 3">
+          <div class="card_image">
+            <img src="https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif" />
+          </div>
+          <div class="card_title">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 4">
+          <div class="card_image">
+            <img src="https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif" />
+          </div>
+          <div class="card_title title-black">
+            <p>Card Title</p>
+          </div>
+        </div>{" "}
+        <div class="musicCard 1">
+          <div class="card_image">
+            <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
+          </div>
+          <div class="card_title title-white">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 2">
+          <div class="card_image">
+            <img src="https://cdn.blackmilkclothing.com/media/wysiwyg/Wallpapers/PhoneWallpapers_FloralCoral.jpg" />
+          </div>
+          <div class="card_title title-white">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 3">
+          <div class="card_image">
+            <img src="https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif" />
+          </div>
+          <div class="card_title">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 4">
+          <div class="card_image">
+            <img src="https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif" />
+          </div>
+          <div class="card_title title-black">
+            <p>Card Title</p>
+          </div>
+        </div>{" "}
+        <div class="musicCard 1">
+          <div class="card_image">
+            <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
+          </div>
+          <div class="card_title title-white">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 2">
+          <div class="card_image">
+            <img src="https://cdn.blackmilkclothing.com/media/wysiwyg/Wallpapers/PhoneWallpapers_FloralCoral.jpg" />
+          </div>
+          <div class="card_title title-white">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 3">
+          <div class="card_image">
+            <img src="https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif" />
+          </div>
+          <div class="card_title">
+            <p>Card Title</p>
+          </div>
+        </div>
+        <div class="musicCard 4">
+          <div class="card_image">
+            <img src="https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif" />
+          </div>
+          <div class="card_title title-black">
+            <p>Card Title</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
