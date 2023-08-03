e04dffbb6d3cc970f188eb71bf7d7ce624955217
nevermindperil <123xxx@kakao.com>
2023-08-03 13:22:18 +0900

    feat:footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "../../css/layout/Footer.css";
 const Footer = () => {
-  return <footer>푸터 입니다람쥐...</footer>;
+  return (
+    <div className="footerWrap">
+      <div>
+        <p>SSAFY 9th Daejeon B110</p>
+        <p>강현곤 | 이대경 | 이상욱 | 이준혁 | 송원규 </p>
+        <p>SING STREET</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
