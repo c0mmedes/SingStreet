26514113ff6345c3dcb59f03a994695ae8956d84
LeeSW-k <swl9455@naver.com>
2023-08-04 00:45:35 +0900

    entCreate

--- frontend/src/components/ent/EntCreate.js
+++ frontend/src/components/ent/EntCreate.js
@@ -11,58 +11,53 @@ const entcreate = () => {
 
           <div className="row clearfix">
             <div className="">
-              <form>
+              <form className="entCreateForm">
+              <label>엔터명</label>
                 <div className="input_field">
                   <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
+                    type="text"
+                    name="entId"
                     required
                   />
                 </div>
                 <div className="input_field">
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
+                  {/* <input type="radio" id="ex_rd" name="ex_rds"> 
+                  <label for="ex_rd">라디오버튼</label> */}
+                </div>
+                <label>엔터 소개</label>
+                <div className="input_field">
+                  <textarea id="entInfo"
+                    type="text"
+                    name="entInfo"
                     required
                   />
                 </div>
+                <label>#테그</label>
                 <div className="input_field">
                   <input
-                    type="password"
-                    name="password"
-                    placeholder="Re-type Password"
+                    type="text"
+                    name="tagList"
+                    placeholder="#뉴진스 #하입보이"
                     required
                   />
                 </div>
-                <div className="row clearfix">
-                  <div className="col_half">
-                    <div className="input_field">
-                      <input type="text" name="name" placeholder="First Name" />
-                    </div>
-                  </div>
-                  <div className="col_half">
-                    <div className="input_field">
-                      <input
-                        type="text"
-                        name="name"
-                        placeholder="Last Name"
-                        required
-                      />
-                    </div>
-                  </div>
-                </div>
-
+                <label>공개여부</label>
                 <div className="input_field select_option">
                   <select>
-                    <option>Select a country</option>
-                    <option>Option 1</option>
-                    <option>Option 2</option>
+                    <option>공개엔터 (바로 가입)</option>
+                    <option>공개엔터 (승인 대기)</option>
                   </select>
                   <div className="select_arrow"></div>
                 </div>
-
+                <label>업로드</label>
+                <div className="input_field">
+                <input
+                    type="file"
+                    name="file"
+                    placeholder="#뉴진스 #하입보이"
+                    required
+                  />
+                </div>
                 <input className="button" type="submit" value="생성하기" />
               </form>
             </div>
