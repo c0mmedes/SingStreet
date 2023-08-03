6ecc6ef602cf282ecf6ce745f295950ae73c8d68
강현곤 <shoot_1@naver.com>
2023-08-03 18:56:35 +0900

    Feat: Header 로그아웃부분(로그아웃시 로그인정보,토큰 날려버림)

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -3,60 +3,68 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../css/layout/Header.css";
 import logo from "../../assets/logo.png";
-import {api} from "../../services/httpService"
+import { api } from "../../services/httpService";
 
-const Header = ({isLogin, setIsLogin, user, setUser}) => {
+const Header = ({ isLogin, setIsLogin, user, setUser }) => {
+	const apiInstance = api();
+	const accessToken = sessionStorage.getItem("accessToken");
+	console.log(accessToken);
 
-  const apiInstance = api(); 
-  const accessToken = sessionStorage.getItem("accessToken");
-  console.log(accessToken);
+	async function onClickLogout() {
+		try {
+			await apiInstance.post(
+				"/auth/logout",
+				{
+					// 요청 바디 데이터 (필요한 경우 추가)
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${accessToken}`, // Bearer 토큰 포함
+					},
+				}
+			);
+			setIsLogin();
+			sessionStorage.removeItem("accessToken");
+			sessionStorage.removeItem("refreshToken");
+			alert("로그아웃 성공");
+		} catch (error) {
+			alert("로그아웃 실패"); // 사용중인건지 사용이 불가능한건지 구분해줘야함
+		}
+	}
 
-  async function onClickLogout (){
-    try {
-      const res = await apiInstance.post('/auth/logout', {
-        // 요청 바디 데이터 (필요한 경우 추가)
-      }, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`, // Bearer 토큰 포함
-        },
-      });
-      alert("로그아웃 성공");
-    } catch (error) {
-      alert("로그아웃 실패"); // 사용중인건지 사용이 불가능한건지 구분해줘야함
-    }
-  };
-
-  return (
-    <header>
-      <Link to="/">
-        <img src={logo} alt="SING STREET 로고" />
-      </Link>
-      <div className="header_right">
-        <Link to="/ent">
-          <span>Ent</span>
-        </Link>
-        <Link to="/chart">
-          <span>Chart</span>
-        </Link>
-        <Link to="/music">
-          <span>Music</span>
-        </Link>
-        {/* 로그인 상태에 따라 Login 또는 Mypage로 링크 변경 */}
-        {(isLogin === true) ? (
-            <div>
-              <Link to="/mypage">
-                <span>Mypage</span>
-              </Link>
-              <a href='#' onClick={onClickLogout}>Logout</a>
-            </div>
-        ) : (
-          <Link to="/login">
-            <span>Login</span>
-          </Link>
-        )}
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<Link to="/">
+				<img src={logo} alt="SING STREET 로고" />
+			</Link>
+			<div className="header_right">
+				<Link to="/ent">
+					<span>Ent</span>
+				</Link>
+				<Link to="/chart">
+					<span>Chart</span>
+				</Link>
+				<Link to="/music">
+					<span>Music</span>
+				</Link>
+				{/* 로그인 상태에 따라 Login 또는 Mypage로 링크 변경 */}
+				{isLogin === true ? (
+					<div>
+						<Link to="/mypage">
+							<span>Mypage</span>
+						</Link>
+						<a href="#" onClick={onClickLogout}>
+							Logout
+						</a>
+					</div>
+				) : (
+					<Link to="/login">
+						<span>Login</span>
+					</Link>
+				)}
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- frontend/src/containers/user/HeaderContainer.js
+++ frontend/src/containers/user/HeaderContainer.js
@@ -2,31 +2,28 @@
 // Header 컴포넌트를 리덕스와 연결하여 리덕스 액션을 props로 전달하는 역할.
 // mapDispatchToProps 객체를 통해 setUser 액션 생성자 함수를 props로 매핑해줌.
 import React from "react";
-import {connect} from "react-redux";
-import {setUser, setIsLogin} from "../../modules/user/user";
+import { connect } from "react-redux";
+import { setUser, setIsLogin } from "../../modules/user/user";
 import Header from "../../components/layout/Header";
 
-const HeaderContainer = ({isLogin}) => {
-    console.log(isLogin);
-    return(
-        <Header isLogin={isLogin}></Header>
-    );
-    
+const HeaderContainer = ({ isLogin, setIsLogin }) => {
+	console.log(isLogin);
+	return <Header isLogin={isLogin} setIsLogin={setIsLogin}></Header>;
 };
 
-const mapStateToProps = state => ({
-    user: state.user.user,
-    isLogin: state.user.isLogin,
+const mapStateToProps = (state) => ({
+	user: state.user.user,
+	isLogin: state.user.isLogin,
 });
-const mapDispatchToProps = dispatch => ({
-    setUser: () =>{
-        dispatch(setUser());
-    },
-    setIsLogin: () =>{
-        dispatch(setIsLogin());
-    },
+const mapDispatchToProps = (dispatch) => ({
+	setUser: () => {
+		dispatch(setUser());
+	},
+	setIsLogin: () => {
+		dispatch(setIsLogin());
+	},
 });
 
-// Login 컴포넌트에선 dispatch만 사용하고 리덕스 '스토어의 상태'를 사용하지않음 
+// Login 컴포넌트에선 dispatch만 사용하고 리덕스 '스토어의 상태'를 사용하지않음
 // => null
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderContainer);
