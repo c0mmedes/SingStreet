7758e01efd2d0c2df063e3174074b0576f21cf60
강현곤 <shoot_1@naver.com>
2023-08-03 16:16:22 +0900

    persist를 sessionstorage로 변경2

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import sessionStorage from "redux-persist/es/storage/session";
+// import storage from "redux-persist/lib/storage";
 import user from './user/user';
 
 const userPersistConfig = {
