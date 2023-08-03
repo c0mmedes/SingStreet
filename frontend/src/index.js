b64a2c6ba96508dbd134a61fc9080acf5b416abd
강현곤 <shoot_1@naver.com>
2023-08-04 04:52:44 +0900

    Refactor: container 디렉토리 구조변경

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import {createStore} from 'redux';
+import { createStore } from "redux";
 import { persistStore } from "redux-persist";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 import App from "./App";
-import HeaderContainer from "./containers/user/HeaderContainer";
+import HeaderContainer from "./containers/layout/HeaderContainer";
 import Footer from "./components/layout/Footer.js";
 import rootReducer from "./modules/index";
-import { composeWithDevTools } from "@redux-devtools/extension"
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { PersistGate } from "redux-persist/integration/react";
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer, composeWithDevTools());
 const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <HeaderContainer />
-        <App />
-        {/* <Footer/> */}
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
+	<BrowserRouter>
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<HeaderContainer />
+				<App />
+				{/* <Footer/> */}
+			</PersistGate>
+		</Provider>
+	</BrowserRouter>
 );
-
