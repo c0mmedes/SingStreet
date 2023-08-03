6529db66eeada66d2f8a84921de60b8699e2685c
강현곤 <shoot_1@naver.com>
2023-08-03 14:39:12 +0900

    Feat: redux persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,21 +3,26 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import { persistStore } from "redux-persist"; // load
+import { PersistGate } from "redux-persist/integration/react"; // load
 import App from "./App";
 import HeaderContainer from "./containers/user/HeaderContainer";
 import Footer from "./components/layout/Footer.js";
-import rootReducer from "./modules/index";
+import persistedReducer from "./modules/index";
 import { composeWithDevTools } from "@redux-devtools/extension"
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(persistedReducer, composeWithDevTools())
+const persistor = persistStore(store); // 정의
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <HeaderContainer />
-      <App />
-      {/* <Footer/> */}
-    </Provider>
+      <PersistGate persistor={persistor}>
+        <HeaderContainer />
+        <App />
+        {/* <Footer/> */}
+      </PersistGate>
+      </Provider>
   </BrowserRouter>
 );
--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -1,8 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist"; // load
+import storageSession from "redux-persist/lib/storage/session";  // sessionStorage
+// import storage from 'redux-persist/lib/storage // localstorage
 import user from './user/user';
+
+const persistConfig = {
+    key: "root",
+    storage: storageSession, // 사용할 스토리지를 정의해요.
+    whitelist: ["isLogin"], // 유지 할 데이터를 정의해요
+  };
 
 const rootReducer = combineReducers({
     user,
 });
 
-export default rootReducer;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+// reducer와 위에서 설정 한 persist설정을 합쳐요
+
+export default persistedReducer;
