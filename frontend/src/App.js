ba9a46c4c3fa147da5c1bff9cd27eee54b257837
강현곤 <shoot_1@naver.com>
2023-08-04 13:00:31 +0900

    Feat: 라우터가드

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, Navigate  } from "react-router-dom";
 import Ent from "./components/ent/Ent";
 import Home from "./components/Home";
 import Chart from "./components/chart/Chart";
@@ -6,23 +6,28 @@ import Music from "./components/work/Music";
 import LoginContainer from "./containers/user/LoginContainer";
 import Register from "./components/user/Register";
 import Mypage from "./components/user/Mypage";
-import React, { useState } from "react";
+import React from "react";
 import Entcreate from "./components/ent/EntCreate";
 import Entmain from "./components/ent/EntMain";
 import "./css/App.css";
+import { connect } from "react-redux";
 
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 저장하는 state
+const mapStateToProps = (state) => ({
+	userInfo: state.user.userInfo,
+	isLogin: state.user.isLogin,
+});
 
-  // 로그인 성공 시 호출되는 함수
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true);
-  };
 
-  //   // 로그아웃 시 호출되는 함수
-  //   const handleLogout = () => {
-  //     setIsLoggedIn(false);
-  //   };
+function PrivateRoute({ component: Component, isLogin, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      element={isLogin ? <Component /> : <Navigate to="/login" />}
+    />
+  );
+}
+
+function App({ isLogin }) {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -30,15 +35,13 @@ function App() {
       <Route path="/chart" element={<Chart />} />
       <Route path="/music" element={<Music />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/login"
-        element={<LoginContainer onLoginSuccess={handleLoginSuccess} />}
-      />
-      <Route path="/mypage" element={<Mypage isLoggedIn={isLoggedIn} />} />
-      <Route path="/entcreate" element={<Entcreate />} />
+      <Route path="/login" element={<LoginContainer />} />
+      <PrivateRoute path="/mypage" element={<Mypage />} isLogin={isLogin} />
+      <PrivateRoute path="/entcreate" element={<Entcreate />} isLogin={isLogin} />
       <Route path="/entmain" element={<Entmain />} />
     </Routes>
   );
 }
 
-export default App;
+export default connect(mapStateToProps, null)(App);
+
