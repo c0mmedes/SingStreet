dba7e1ca6897e249edc5a40de2b8fa4e0ba241d8
nevermindperil <123xxx@kakao.com>
2023-08-04 13:29:47 +0900

    api서버의 기본 url 변경

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 function api() {
-	const instance = axios.create({
-		baseURL: "http://i9b110.p.ssafy.io:4050", // API 서버의 기본 URL
-		timeout: 5000, // 요청 시간 초과 설정 (옵션)
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-	return instance;
+  const instance = axios.create({
+    baseURL: "https://i9b110,p.ssafy.io/backend", // API 서버의 기본 URL
+
+    timeout: 5000, // 요청 시간 초과 설정 (옵션)
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return instance;
 }
 
 export { api };
