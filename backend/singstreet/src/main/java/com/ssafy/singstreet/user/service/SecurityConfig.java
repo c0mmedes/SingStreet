4f76b818e4def05f7b354cbde833342c7f6ad0ae
이준혁 <human646581@gmail.com>
2023-08-04 13:39:45 +0900

    Update backend/singstreet/src/main/java/com/ssafy/singstreet/user/service/SecurityConfig.java

--- backend/singstreet/src/main/java/com/ssafy/singstreet/user/service/SecurityConfig.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/user/service/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://i9b110.p.ssafy.io:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://i9b110.p.ssafy.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
