716019a50b72e0f90cbd5e8879b18315ec57aac4
강현곤 <shoot_1@naver.com>
2023-08-05 20:32:06 +0900

    Fix: 엔터목록 불러오기진행중...

--- frontend/src/components/ent/Ent.js
+++ frontend/src/components/ent/Ent.js
@@ -65,69 +65,6 @@ const Ent = () => {
 					</li>
 				))}
 			</ul>
-			<ul className="cards">
-				<li>
-					<a href="" class="card">
-						<img src={myImage} class="card__image" alt="" />
-						<div class="card__overlay">
-							<div class="card__header">
-								<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-									<path />
-								</svg>
-								<img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
-								<div class="card__header-text">
-									<h3 class="card__title">B110 엔터</h3>
-									<span class="card__status"># KPOP # 뉴진스 # 10대 # 화이팅</span>
-								</div>
-							</div>
-							<p class="card__description">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="" class="card">
-						<img src={myImage2} class="card__image" alt="" />
-						<div class="card__overlay">
-							<div class="card__header">
-								<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-									<path />
-								</svg>
-								<img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
-								<div class="card__header-text">
-									<h3 class="card__title">kim Cattrall</h3>
-									<span class="card__status">3 hours ago</span>
-								</div>
-							</div>
-							<p class="card__description">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="" class="card">
-						<img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-						<div class="card__overlay">
-							<div class="card__header">
-								<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-									<path />
-								</svg>
-								<img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
-								<div class="card__header-text">
-									<h3 class="card__title">kim Cattrall</h3>
-									<span class="card__status">3 hours ago</span>
-								</div>
-							</div>
-							<p class="card__description">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
-							</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-
 			<div className="entCreatebtnWrap">
 				<Link to="/entcreate">
 					<button className="entCreatebtn">엔터 생성하기</button>
