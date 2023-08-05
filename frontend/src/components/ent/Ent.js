a8ab810146014bde4144a66520533168ab03ea2c
강현곤 <shoot_1@naver.com>
2023-08-05 20:27:40 +0900

    Feat: 엔터목록 불러오기진행중...

--- frontend/src/components/ent/Ent.js
+++ frontend/src/components/ent/Ent.js
@@ -1,201 +1,141 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../css/ent/Ent.css";
 import myImage from "../../assets/entLogo2.png";
 import myImage2 from "../../assets/entLogo1.PNG";
 import Footer from "../layout/Footer.js";
 import { Link } from "react-router-dom";
+import { api } from "../../services/httpService";
+
 const Ent = () => {
-  return (
-    <div>
-      <form className="entSearchWrap">
-        <input
-          placeholder="마음에 드는 엔터를 검색해보세요. . ."
-          type="text"
-          className="entSearchInput"></input>
-        <input type="submit" value="검색" className="entSearchSubmit"></input>
-        <select>
-          <option value="allEnt">전체</option>
-          <option value="myEnt">가입된 엔터</option>
-        </select>
-      </form>
+	const [entList, setEntList] = useState([]);
+	const [page, setPage] = useState(0);
+	// axios 객체
+	const apiInstance = api();
 
-      <ul className="cards">
-        <li>
-          <a href="#" class="card">
-            <Link to="/entmain">
-              <img src={myImage} class="card__image" alt="" />
-              <div class="card__overlay">
-                <div class="card__header">
-                  <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                    <path />
-                  </svg>
-                  <img
-                    class="card__thumb"
-                    src="https://i.imgur.com/7D7I6dI.png"
-                    alt=""
-                  />
-                  <div class="card__header-text">
-                    <h3 class="card__title">B110 엔터</h3>
-                    <span class="card__status">
-                      # KPOP # 뉴진스 # 10대 # 화이팅
-                    </span>
-                  </div>{" "}
-                </div>
-                <p class="card__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Asperiores, blanditiis?
-                </p>
-              </div>{" "}
-            </Link>
-          </a>
-        </li>
-        <li>
-          <a href="" class="card">
-            <img src={myImage2} class="card__image" alt="" />
-            <div class="card__overlay">
-              <div class="card__header">
-                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                  <path />
-                </svg>
-                <img
-                  class="card__thumb"
-                  src="https://i.imgur.com/sjLMNDM.png"
-                  alt=""
-                />
-                <div class="card__header-text">
-                  <h3 class="card__title">kim Cattrall</h3>
-                  <span class="card__status">3 hours ago</span>
-                </div>
-              </div>
-              <p class="card__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, blanditiis?
-              </p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="" class="card">
-            <img
-              src="https://i.imgur.com/2DhmtJ4.jpg"
-              class="card__image"
-              alt=""
-            />
-            <div class="card__overlay">
-              <div class="card__header">
-                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                  <path />
-                </svg>
-                <img
-                  class="card__thumb"
-                  src="https://i.imgur.com/sjLMNDM.png"
-                  alt=""
-                />
-                <div class="card__header-text">
-                  <h3 class="card__title">kim Cattrall</h3>
-                  <span class="card__status">3 hours ago</span>
-                </div>
-              </div>
-              <p class="card__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, blanditiis?
-              </p>
-            </div>
-          </a>
-        </li>
-      </ul>
-      <ul className="cards">
-        <li>
-          <a href="" class="card">
-            <img src={myImage} class="card__image" alt="" />
-            <div class="card__overlay">
-              <div class="card__header">
-                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                  <path />
-                </svg>
-                <img
-                  class="card__thumb"
-                  src="https://i.imgur.com/7D7I6dI.png"
-                  alt=""
-                />
-                <div class="card__header-text">
-                  <h3 class="card__title">B110 엔터</h3>
-                  <span class="card__status">
-                    # KPOP # 뉴진스 # 10대 # 화이팅
-                  </span>
-                </div>
-              </div>
-              <p class="card__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, blanditiis?
-              </p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="" class="card">
-            <img src={myImage2} class="card__image" alt="" />
-            <div class="card__overlay">
-              <div class="card__header">
-                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                  <path />
-                </svg>
-                <img
-                  class="card__thumb"
-                  src="https://i.imgur.com/sjLMNDM.png"
-                  alt=""
-                />
-                <div class="card__header-text">
-                  <h3 class="card__title">kim Cattrall</h3>
-                  <span class="card__status">3 hours ago</span>
-                </div>
-              </div>
-              <p class="card__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, blanditiis?
-              </p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="" class="card">
-            <img
-              src="https://i.imgur.com/2DhmtJ4.jpg"
-              class="card__image"
-              alt=""
-            />
-            <div class="card__overlay">
-              <div class="card__header">
-                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                  <path />
-                </svg>
-                <img
-                  class="card__thumb"
-                  src="https://i.imgur.com/sjLMNDM.png"
-                  alt=""
-                />
-                <div class="card__header-text">
-                  <h3 class="card__title">kim Cattrall</h3>
-                  <span class="card__status">3 hours ago</span>
-                </div>
-              </div>
-              <p class="card__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, blanditiis?
-              </p>
-            </div>
-          </a>
-        </li>
-      </ul>
+	const getEntList = async () => {
+		const res = await apiInstance.get(`/ent?page=${page}&size=20`);
+		setEntList(res.data.content);
+		console.log(entList);
+	};
 
-      <div className="entCreatebtnWrap">
-        <Link to="/entcreate">
-          <button className="entCreatebtn">엔터 생성하기</button>
-        </Link>
-      </div>
-      <Footer />
-    </div>
-  );
+	useEffect(() => {
+		getEntList();
+	}, []);
+
+	return (
+		<div>
+			<form className="entSearchWrap">
+				<input
+					placeholder="마음에 드는 엔터를 검색해보세요. . ."
+					type="text"
+					className="entSearchInput"
+				></input>
+				<input type="submit" value="검색" className="entSearchSubmit"></input>
+				<select>
+					<option value="allEnt">전체</option>
+					<option value="myEnt">가입된 엔터</option>
+				</select>
+			</form>
+
+			<ul className="cards">
+				{entList.map((ent) => (
+					<li key={ent.entId}>
+						<a href="#" class="card">
+							<Link to="/entmain">
+								<img src={myImage} class="card__image" alt="" />
+								<div class="card__overlay">
+									<div class="card__header">
+										<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+											<path />
+										</svg>
+										<img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+										<div class="card__header-text">
+											<h3 class="card__title">{ent.entName}</h3>
+											<span class="card__status">
+												{ent.tagNameList.map((tagName) => (
+													<b>#{tagName}</b>
+												))}
+											</span>
+										</div>
+									</div>
+									<p class="card__description">{ent.entInfo}</p>
+								</div>
+							</Link>
+						</a>
+					</li>
+				))}
+			</ul>
+			<ul className="cards">
+				<li>
+					<a href="" class="card">
+						<img src={myImage} class="card__image" alt="" />
+						<div class="card__overlay">
+							<div class="card__header">
+								<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+									<path />
+								</svg>
+								<img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+								<div class="card__header-text">
+									<h3 class="card__title">B110 엔터</h3>
+									<span class="card__status"># KPOP # 뉴진스 # 10대 # 화이팅</span>
+								</div>
+							</div>
+							<p class="card__description">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+							</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="" class="card">
+						<img src={myImage2} class="card__image" alt="" />
+						<div class="card__overlay">
+							<div class="card__header">
+								<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+									<path />
+								</svg>
+								<img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+								<div class="card__header-text">
+									<h3 class="card__title">kim Cattrall</h3>
+									<span class="card__status">3 hours ago</span>
+								</div>
+							</div>
+							<p class="card__description">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+							</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="" class="card">
+						<img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+						<div class="card__overlay">
+							<div class="card__header">
+								<svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+									<path />
+								</svg>
+								<img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+								<div class="card__header-text">
+									<h3 class="card__title">kim Cattrall</h3>
+									<span class="card__status">3 hours ago</span>
+								</div>
+							</div>
+							<p class="card__description">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+							</p>
+						</div>
+					</a>
+				</li>
+			</ul>
+
+			<div className="entCreatebtnWrap">
+				<Link to="/entcreate">
+					<button className="entCreatebtn">엔터 생성하기</button>
+				</Link>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Ent;
