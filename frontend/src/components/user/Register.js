af6e7f8f2880bcf76c2e03c0fc75a92413965ca1
nevermindperil <123xxx@kakao.com>
2023-08-07 15:49:39 +0900

    register 페이지 레이아웃 수정

--- frontend/src/components/user/Register.js
+++ frontend/src/components/user/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../css/user/Register.css";
 import { api } from "../../services/httpService";
+import Footer from "../layout/Footer.js";
 
 function Register() {
   const [nickname, setNickname] = useState("");
@@ -147,80 +148,86 @@ function Register() {
   }
 
   return (
-    <div className="RegisterWrap">
-      <div>
-        <h1>REGISTER</h1>
-        <div className="inputbox">
-          <label htmlFor="nickname">Nickname</label>
-          <input
-            type="text"
-            name="nickname"
-            value={nickname}
-            onChange={handleNickname}
-          />
-          <button onClick={checkDuplicateNickname} className="CheckBtn">
-            중복확인
-          </button>
-        </div>
-        <div className="inputbox">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleEmail}
-          />
-          <button onClick={checkDuplicateEmail} className="CheckBtn">
-            중복확인
-          </button>
-        </div>
-        {/* 인증번호 입력란과 확인 버튼 */}
-        {!isAuthorized && isEmailDuplicated && (
+    <div>
+      <div className="RegisterWrap">
+        <div className="RegisterForm">
+          <h1 className="resisterH1">REGISTER</h1>
           <div className="inputbox">
-            <label htmlFor="authCode">Verification Code</label>
+            <label htmlFor="nickname">Nickname</label>
             <input
               type="text"
-              name="authCode"
-              value={authCode}
-              onChange={handleAuthCode}
+              name="nickname"
+              value={nickname}
+              onChange={handleNickname}
+              className="RegisterInput"
             />
-            <button onClick={onClickAuthorize} className="CheckBtn">
-              확인
+            <button onClick={checkDuplicateNickname} className="CheckBtn ">
+              중복확인
             </button>
           </div>
-        )}
-        <div className="inputbox">
-          <label htmlFor="gender">Gender</label>
-          <select name="gender" value={gender} onChange={handleGender}>
-            <option value="">Select Gender</option>
-            <option value="남">남</option>
-            <option value="녀">녀</option>
-          </select>
+          <div className="inputbox">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmail}
+              className="RegisterInput"
+            />
+            <button onClick={checkDuplicateEmail} className="CheckBtn">
+              중복확인
+            </button>
+          </div>
+          {/* 인증번호 입력란과 확인 버튼 */}
+          {!isAuthorized && isEmailDuplicated && (
+            <div className="inputbox">
+              <label htmlFor="authCode">Verification Code</label>
+              <input
+                type="text"
+                name="authCode"
+                value={authCode}
+                onChange={handleAuthCode}
+                className="RegisterInput"
+              />
+              <button onClick={onClickAuthorize} className="CheckBtn">
+                확인
+              </button>
+            </div>
+          )}
+          <div className="inputbox">
+            <label htmlFor="gender">Gender</label>
+            <select name="gender" value={gender} onChange={handleGender}>
+              <option value="">Select Gender</option>
+              <option value="남">남</option>
+              <option value="녀">녀</option>
+            </select>
+          </div>{" "}
+          <div className="inputbox">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={handlePassword}
+              className="RegisterInput"
+            />
+          </div>
+          <div className="inputbox">
+            <label htmlFor="passwordConfirm">Password Confirm</label>
+            <input
+              type="password"
+              name="passwordConfirm"
+              value={passwordConfirm}
+              onChange={handlePasswordConfirm}
+              className="RegisterInput"
+            />
+          </div>
+          <button type="button" onClick={onClickRegister} className="signBtn">
+            Sign up
+          </button>
         </div>
       </div>
-      <div className="rightWrap">
-        <div className="inputbox">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handlePassword}
-          />
-        </div>
-        <div className="inputbox">
-          <label htmlFor="passwordConfirm">Password Confirm</label>
-          <input
-            type="password"
-            name="passwordConfirm"
-            value={passwordConfirm}
-            onChange={handlePasswordConfirm}
-          />
-        </div>
-        <button type="button" onClick={onClickRegister} className="signBtn">
-          Sign up
-        </button>
-      </div>
+      <Footer />
     </div>
   );
 }
