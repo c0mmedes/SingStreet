294ff7f665822e5ac3696ddf34949562df8d53fa
강현곤 <shoot_1@naver.com>
2023-08-07 15:52:21 +0900

    Test: 엔터페이지 조건부렌더링

--- frontend/src/components/layout/EntNav.js
+++ frontend/src/components/layout/EntNav.js
@@ -1,7 +1,7 @@
 //src/components/layout/EntNav.js
 
-import React,{useState} from "react";
-import { Link, useParams } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import "../../css/layout/EntNav.css";
 const EntNav = ({entId, entMasterId, entName, userInfo}) => {
 	/* "entId": 1,
@@ -18,7 +18,7 @@ const EntNav = ({entId, entMasterId, entName, userInfo}) => {
 					<div class="side-menu">
 						<div className="side-title side-title1 ">
 							userId: {userInfo.userId} entMasterId: {entMasterId}
-							{userInfo.userId === entMasterId? (
+							{parseInt(userInfo.userId) === parseInt(entMasterId)? (
 							<Link to={`/entapplicants/${entId}/${entName}`}>
 								<a className="menuLink" href="#">
 									<span>지원자 목록</span>
