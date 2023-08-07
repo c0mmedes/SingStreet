ca81da706a9c894c3903c9faa2906a5282132e93
이준혁 <human646581@gmail.com>
2023-08-07 17:43:25 +0900

    Update backend/singstreet/src/main/java/com/ssafy/singstreet/user/model/UserRegistDTO.java

--- backend/singstreet/src/main/java/com/ssafy/singstreet/user/model/UserRegistDTO.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/user/model/UserRegistDTO.java
@@ -1,52 +1,20 @@
 package com.ssafy.singstreet.user.model;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Getter
+@Setter
 public class UserRegistDTO {
     private String nickname;
     private String userImg;
     private String email;
     private Character gender;
     private String password;
+    private MultipartFile multipartFile;
 
-    // Add getters and setters for all fields
 
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getUserImg() {
-        return userImg;
-    }
-
-    public void setUserImg(String userImg) {
-        this.userImg = userImg;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Character getGender() {
-        return gender;
-    }
-
-    public void setGender(Character gender) {
-        this.gender = gender;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
