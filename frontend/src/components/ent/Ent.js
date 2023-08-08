62f169c980bc2656336ca23b24a2e6021f50d1dc
강현곤 <shoot_1@naver.com>
2023-08-08 16:35:51 +0900

    fix: 나의사람들과

--- frontend/src/components/ent/Ent.js
+++ frontend/src/components/ent/Ent.js
@@ -37,8 +37,14 @@ const Ent = () => {
     setIsLastPage(res.data.last);
     setPage(nextPage);
 
-    // 스크롤을 내려가도록 설정
-    scrollRef.current.scrollIntoView({ behavior: 'smooth'});
+     // 스크롤을 새로 추가된 데이터의 끝으로 이동하도록 설정
+    if (scrollRef.current) {
+      const newEntIndex = newEntList.length - res.data.content.length;
+      const newEntElement = document.querySelector(`#ent-${newEntIndex}`);
+      if (newEntElement) {
+        newEntElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      }
+    }
   };
 
 /*   useEffect(() => {
