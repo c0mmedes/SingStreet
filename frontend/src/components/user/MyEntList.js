956cf70676fb01c4e130a37491419ac1c2134ef4
강현곤 <shoot_1@naver.com>
2023-08-08 17:54:44 +0900

    가기전 마지막

--- frontend/src/components/user/MyEntList.js
+++ frontend/src/components/user/MyEntList.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "../../css/user/Mypage.css";
 import { api } from "../../services/httpService";
-
+import { useNavigate } from "react-router-dom";
 const MyEntList = ({ isLogin, userInfo, addUserInfo, setIsLogin, myEntList, addToMyEntList }) => {
     // axios 인스턴스 생성
     const apiInstance = api();
+    // 페이지 이동을 위한 navigate 생성
+    const navigate = useNavigate();
 
     console.log(userInfo);
     return (
         <div>
         {myEntList.map((myEnt) => (
             <li key={myEnt.entId}>
+                <div>
                     <h3 class="card__title">{myEnt.entName}</h3>
                     <h3 class="card__title">{myEnt.entInfo}</h3>
                     <h3 class="card__title">{myEnt.entImg}</h3>
+                </div>
             </li>
         ))}
     </div>
