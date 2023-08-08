52cf736fc81202165511b0f800d01c79e4514d45
이대경 <eorud2546@nate.com>
2023-08-08 17:54:15 +0900

    Update SecurityConfig.java

--- backend/singstreet/src/main/java/com/ssafy/singstreet/user/service/SecurityConfig.java
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/user/service/SecurityConfig.java
@@ -31,9 +31,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://i9b110.p.ssafy.io:3000"));
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-//        configuration.setAllowedOrigins(Arrays.asList("https://i9b110.p.ssafy.io"));
+        configuration.setAllowedOrigins(Arrays.asList("http://i9b110.p.ssafy.io:3000"));
+        // configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://i9b110.p.ssafy.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
