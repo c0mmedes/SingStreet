ba711762dcbdd26d22cfb95410c1b13b3e80175a
nevermindperil <123xxx@kakao.com>
2023-08-09 09:20:34 +0900

    엔터지원가목록

---
+++ frontend/src/containers/layout/MypageNav.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../css/layout/MypageNav.css";
+import { api } from "../../services/httpService";
+import { useNavigate } from "react-router-dom";
+const MypageNav = ({}) => {
+  return <div></div>;
+};
+
+export default MypageNav;
