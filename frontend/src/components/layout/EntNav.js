f79b1b2d61d39b4e6cae7c431c59e066c5526d61
nevermindperil <123xxx@kakao.com>
2023-08-09 17:31:17 +0900

    송원규5_21

--- frontend/src/components/layout/EntNav.js
+++ frontend/src/components/layout/EntNav.js
@@ -3,50 +3,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../css/layout/EntNav.css";
-const EntNav = ({entId, entMasterId, entName, userInfo}) => {
-	/* "entId": 1,
+const EntNav = ({ entId, entMasterId, entName, userInfo }) => {
+  /* "entId": 1,
 	"userId": "엔터장 아이디",
 	"entName": "qwe",
 	"entImg": "qwe",
 	"entInfo": "qwe",
 	"tagNameList": [],
 	"autoAccepted": true */
-	return (
-		<div>
-			<div class="left-side">
-				<div class="side-wrapper">
-					<div class="side-menu">
-						<div className="side-title side-title1 ">
-							{parseInt(userInfo.userId) === parseInt(entMasterId)? (
-							<Link to={`/entmain/${entId}/${entMasterId}/${entName}/EntApplicants`}>
-								<a className="menuLink" href="#">
-									<span>지원자 목록</span>
-								</a>
-							</Link>
-							):(
-							<Link to={`/entmain/${entId}/${entMasterId}/${entName}/entapply`}>
-								<a className="menuLink" href="#">
-									<span>지원 하기</span>
-								</a>
-							</Link>) }
-						</div>
+  return (
+    <div>
+      <div class="left-side">
+        <div class="side-wrapper">
+          <div class="side-menu">
+            <div className="side-title side-title1 ">
+              {parseInt(userInfo.userId) === parseInt(entMasterId) ? (
+                <Link
+                  to={`/entmain/${entId}/${entMasterId}/${entName}/EntApplicants`}>
+                  <a className="menuLink" href="#">
+                    <span>지원자 목록</span>
+                  </a>
+                </Link>
+              ) : (
+                <Link
+                  to={`/entmain/${entId}/${entMasterId}/${entName}/entapply`}>
+                  <a className="menuLink" href="#">
+                    <span>지원 하기</span>
+                  </a>
+                </Link>
+              )}
+            </div>
 
-						<div className="side-title side-title2 ">
-							<h1>프로젝트</h1>
-							<a href="#">프로젝트 생성</a>
-							<a href="#">프로젝트 목록</a>
-						</div>
-						<div className="side-title side-title3 ">
-							<a href="#">
-								<span>작품 목록</span>
-							</a>
-						</div>
-						<button class="entNavBtn show-more">탈퇴하기</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <div className="side-title side-title2 ">
+              <h1>프로젝트</h1>
+              <Link
+                to={`/entmain/${entId}/${entMasterId}/${entName}/entprojectcreate`}>
+                <a className="menuLink" href="#">
+                  프로젝트 생성
+                </a>
+              </Link>
+
+              <a href="#">프로젝트 목록</a>
+            </div>
+            <div className="side-title side-title3 ">
+              <a href="#">
+                <span>작품 목록</span>
+              </a>
+            </div>
+            <button class="entNavBtn show-more">탈퇴하기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EntNav;
