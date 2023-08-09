a6ef144f45a415634c24079872ff148ef97a30d8
nevermindperil <123xxx@kakao.com>
2023-08-09 09:29:53 +0900

    엔터지원가목록

--- frontend/src/components/user/Mypage.js
+++ frontend/src/components/user/Mypage.js
@@ -12,29 +12,30 @@ const Mypage = ({ isLogin, userInfo, addUserInfo, setIsLogin }) => {
       <div className="mypage-title">내 정보</div>
 
       <div className="user-my-info-wrap">
-        <div className="info-item">
-          <label className="info-label">이메일</label>
-          <div className="info-value">{userInfo.email}</div>
+        <div className="myinfoLeft">
+          <div className="info-item">
+            <label className="info-label">프로필</label>
+            <img id="user-profile" src={userInfo.userImg} alt="프로필 이미지" />
+          </div>
         </div>
-
-        <div className="info-item">
-          <label className="info-label">성별</label>
-          <input className="info-input" type="text" value={userInfo.gender} />
-        </div>
-
-        <div className="info-item">
-          <label className="info-label">닉네임</label>
-          <input
-            id="user-nickname"
-            className="info-input"
-            type="text"
-            value={userInfo.nickname}
-          />
-        </div>
-
-        <div className="info-item">
-          <label className="info-label">프로필</label>
-          <img id="user-profile" src={userInfo.userImg} alt="프로필 이미지" />
+        <div className="myinfoRight">
+          <div className="info-item">
+            <label className="info-label">이메일</label>
+            <div className="info-value">{userInfo.email}</div>
+          </div>
+          <div className="info-item">
+            <label className="info-label">성별</label>
+            <input className="info-input" type="text" value={userInfo.gender} />
+          </div>
+          <div className="info-item">
+            <label className="info-label">닉네임</label>
+            <input
+              id="user-nickname"
+              className="info-input"
+              type="text"
+              value={userInfo.nickname}
+            />
+          </div>
         </div>
       </div>
 
