dde4a58fdf1ef28e56397de72505b9b57ac73cfd
nevermindperil <123xxx@kakao.com>
2023-08-11 10:31:23 +0900

    마이페이지 가입된 엔터목록

--- frontend/src/components/ent/EntProjectList.js
+++ frontend/src/components/ent/EntProjectList.js
@@ -8,14 +8,14 @@ const EntProjectList = () => {
   const { entId, entMasterId, entName } = useParams();
   // useState
   const [entProjectList, setEntProjectList] = useState([]);
-   // axios 객체
+  // axios 객체
   const apiInstance = api();
   //useEffect (화면 첫 렌더링시 실행)
-  useEffect(()=>{
+  useEffect(() => {
     getEntProjectList();
-  },[]);
+  }, []);
 
-  // [비동기] 엔터 프로젝트 목록 가져오기 
+  // [비동기] 엔터 프로젝트 목록 가져오기
   const getEntProjectList = async () => {
     const res = await apiInstance.get(`/project/ent/${entId}`);
     console.log(res.data);
@@ -35,21 +35,21 @@ const EntProjectList = () => {
           <option value="myEnt">내 프로젝트</option>
         </select>
       </form>
-      {/* 프로젝트 리스트 */}
-      {entProjectList.map((entProject) => (
-        <div key={entProject.projectId} class="cards-list">
-         <div class="musicCard 1">
-           <div class="card_image">
-             <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
-           </div>
-           <div class="card_title title-white">
-             <p>{entProject.projectName}</p>
-           </div>
-         </div>
-        </div>
-      )
-      )}
-     
+      <div className="entPjtListWrap">
+        {/* 프로젝트 리스트 */}
+        {entProjectList.map((entProject) => (
+          <div key={entProject.projectId} class="cards-list">
+            <div class="musicCard 1">
+              <div class="card_image">
+                <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
+              </div>
+              <div class="card_title title-white">
+                <p>{entProject.projectName}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
