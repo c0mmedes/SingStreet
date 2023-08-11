891b3530842f1fac115605460345ce0df8ba1cb8
강현곤 <shoot_1@naver.com>
2023-08-11 15:47:23 +0900

    feat: Chat 생성

--- frontend/src/containers/chat/ChatContainer.js
+++ frontend/src/containers/chat/ChatContainer.js
@@ -1,6 +1,3 @@
-// EntApplyContainer는
-// EntApply 컴포넌트를 리덕스와 연결하여 리덕스 action,state를 props로 전달하는 역할.
-// mapDispatchToProps,mapStateToProps 객체를 통해 isLogin상태와 ,setIsLogin 액션 생성자 함수를 props로 매핑해줌.
 import React from "react";
 import { connect } from "react-redux";
 import { addUserInfo, setIsLogin } from "../../modules/user/user";
