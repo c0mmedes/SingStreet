91f808c3f5582f92acabaa0ed1c3288cbfdaf354
SongWonkyu <93499344+SongWonkyu@users.noreply.github.com>
2023-08-13 18:18:28 +0900

    프로젝트 상세페이지

--- frontend/src/components/ent/EntProjectMain.js
+++ frontend/src/components/ent/EntProjectMain.js
@@ -3,23 +3,23 @@ import { useParams, Link } from "react-router-dom";
 import { api } from "../../services/httpService";
 
 const EntProjectMain = () => {
-	// useState
-	const [project, setProject] = useState({});
-	// 라우터 경로로 받아오는 정보들
-	const { entId, entMasterId, entName, projectId } = useParams();
-	// 비동기 통신을 위한 apiInstance 생성
-	const apiInstance = api();
-	// useEffect
-	useEffect(() => {
-		getProject();
-	}, []);
+  // useState
+  const [project, setProject] = useState({});
+  // 라우터 경로로 받아오는 정보들
+  const { entId, entMasterId, entName, projectId } = useParams();
+  // 비동기 통신을 위한 apiInstance 생성
+  const apiInstance = api();
+  // useEffect
+  useEffect(() => {
+    getProject();
+  }, []);
 
-	const getProject = async () => {
-		const res = await apiInstance.get(`/project/info/${projectId}`);
-		console.log(res.data);
-		const newProject = { ...res.data }; // 새로운 객체를 생성하고 res.data의 내용을 복사
-		setProject(newProject);
-		/*
+  const getProject = async () => {
+    const res = await apiInstance.get(`/project/info/${projectId}`);
+    console.log(res.data);
+    const newProject = { ...res.data }; // 새로운 객체를 생성하고 res.data의 내용을 복사
+    setProject(newProject);
+    /*
             projectId
             userId // 프로젝트장
             projectName
@@ -35,50 +35,59 @@ const EntProjectMain = () => {
             }
             isRecruited // 모집여부
         */
-	};
-	// 프로젝트 삭제
-	const onClickDeleteProject = async () => {
-		const accessToken = sessionStorage.getItem("accessToken");
-		try {
-			await apiInstance.put(`/project/delete/${projectId}`, {
-				headers: {
-					Authorization: `Bearer ${accessToken}`, // Bearer 토큰 포함
-				},
-			});
-			alert("삭제 성공");
-		} catch {
-			alert("삭제 실패");
-		}
-	};
+  };
+  // 프로젝트 삭제
+  const onClickDeleteProject = async () => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    try {
+      await apiInstance.put(`/project/delete/${projectId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`, // Bearer 토큰 포함
+        },
+      });
+      alert("삭제 성공");
+    } catch {
+      alert("삭제 실패");
+    }
+  };
 
-	return (
-		<div>
-			<Link to={`/entproject/studio/${entId}/${entMasterId}/${entName}/${projectId}`}>
-				<div>스튜디오 입장</div>
-			</Link>
-			<img src={project.projectImg} class="card__image" alt="" />
-			<h3 class="card__title">프로젝트 명: {project.projectName}</h3>
-			{project.isRecruited ? <h3>모집 중</h3> : <h3>모집 마감</h3>}
-			<h3>singName: {project.singName}</h3>
-			<h3>singerName: {project.singerName}</h3>
-			<h3>projectInfo: {project.projectInfo}</h3>
-			<span class="card__status">
-				{project.partList ? (
-					project.partList.map((part) => (
-						<b>
-							파트 : {part.partName} - {part.nickname}
-						</b>
-					))
-				) : (
-					<div>파트가 없습니다.</div>
-				)}
-			</span>
-			<div onClick={onClickDeleteProject}>프로젝트 삭제</div>
-			<Link to={`/entmain/${entId}/${entMasterId}/${entName}/entprojectmodify/${projectId}`}>
-				<div>프로젝트 수정</div>
-			</Link>
-		</div>
-	);
+  return (
+    <div>
+      <div className="pjtMainHeader">
+        <img src={project.projectImg} class="card__image" alt="" />
+        <div className="pjtMainHeaderRight">
+          {project.isRecruited ? <h3>[모집 중]</h3> : <h3>[모집 마감]</h3>}
+          <h3 class="card__title">프로젝트 명: {project.projectName}</h3>
+          <div className="pjtMainHeaderRight_btn">
+            <h3>singerName: {project.singerName}</h3>
+            <h3>singName: {project.singName}</h3>
+          </div>
+        </div>
+      </div>
+      <Link
+        to={`/entproject/studio/${entId}/${entMasterId}/${entName}/${projectId}`}>
+        <div>스튜디오 입장</div>
+      </Link>
+
+      <h3>projectInfo: {project.projectInfo}</h3>
+      <span class="card__status">
+        {project.partList ? (
+          project.partList.map((part) => (
+            <b>
+              파트 : {part.partName} - {part.nickname}
+            </b>
+          ))
+        ) : (
+          <div>파트가 없습니다.</div>
+        )}
+      </span>
+      <div onClick={onClickDeleteProject}>프로젝트 삭제</div>
+      <Link
+        to={`/entmain/${entId}/${entMasterId}/${entName}/entprojectmodify/${projectId}`}>
+        <div>프로젝트 수정</div>
+      </Link>
+    </div>
+  );
 };
 
 export default EntProjectMain;
