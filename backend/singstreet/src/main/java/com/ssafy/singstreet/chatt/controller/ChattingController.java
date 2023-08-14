790119463b0c04fc3d39923893af5f96fdba112d
LeeSW-k <swl9455@naver.com>
2023-08-15 05:05:19 +0900

    feat : chatting setting

---
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/chatt/controller/ChattingController.java
@@ -0,0 +1,30 @@
+package com.ssafy.singstreet.chatt.controller;
+
+import com.ssafy.singstreet.chatt.db.ChatMessage;
+import com.ssafy.singstreet.chatt.service.ChatMessageService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ChattingController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final ChatMessageService messageService;
+
+    @GetMapping("/chatting/{entId}")
+    public ResponseEntity<Slice<ChatMessage>> read(@PathVariable int entId, @RequestParam int page, @RequestParam int size){
+        log.debug("[readChattPage]");
+
+        return new ResponseEntity(messageService.getMessagesWithPagination(entId,page,size), HttpStatus.OK);
+    }
+
+}
---
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/chatt/db/ChatMessage.java
@@ -0,0 +1,35 @@
+package com.ssafy.singstreet.chatt.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.EntityListeners;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)  //Auditing기능(시간 자동으로 값 넣어주는..) 포함
+@Document(collection = "messages")
+public class ChatMessage {
+    @Id
+    private String id;
+    private int entId;
+    private String nickname;
+    private String content;
+    @CreatedDate
+    private Date createdAt;
+
+    public void updateDate(){
+        this.createdAt = new Date();
+    }
+}
