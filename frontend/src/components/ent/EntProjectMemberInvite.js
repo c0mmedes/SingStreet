9b2fd7c49995afe235fb4f5527257bea2f67a09b
강현곤 <shoot_1@naver.com>
2023-08-14 16:56:47 +0900

    Feat: 선택된 멤버 초대

--- frontend/src/components/ent/EntProjectMemberInvite.js
+++ frontend/src/components/ent/EntProjectMemberInvite.js
@@ -12,6 +12,8 @@ const EntProjectMemberInvite = ({ myEntList, userInfo }) => {
   // 상태관리
   // 엔터 멤버 리스트
   const [entMemberList, setEntMemberList] = useState([]);
+  // 선택된 멤버
+  const [selectedMembers, setSelectedMembers] = useState([]);
 
   useEffect(() => {
     // 원하는 조건을 확인하고 이전 화면으로 돌아가기
@@ -50,24 +52,49 @@ const EntProjectMemberInvite = ({ myEntList, userInfo }) => {
     }
   };
 
-  // 초대하기
+  const handleMemberSelection = (userId) => {
+    if (selectedMembers.includes(userId)) {
+      setSelectedMembers(selectedMembers.filter((id) => id !== userId));
+    } else {
+      setSelectedMembers([...selectedMembers, userId]);
+    }
+  };
+
   const onClickInviteButton = async () => {
     try {
-        const accessToken = sessionStorage.getItem("accessToken");
-        const res = await apiInstance.get(`ent/member/}`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`, // Bearer 토큰 포함
-          },
-        });
-        console.log(res.data);
-      } catch {
-        alert("엔터 멤버 목록 불러오기 실패!");
-      }
+      const accessToken = sessionStorage.getItem("accessToken");
+      // selectedMembers에 선택된 멤버들의 userId가 들어있습니다.
+      // 이 정보를 이용하여 초대 등의 작업을 진행할 수 있습니다.
+      console.log("Selected Members: ", selectedMembers);
+      // 선택된 멤버들을 서버로 전송하거나 필요한 동작을 수행할 수 있습니다.
+      apiInstance.post(`project/member/`, { selectedMembers }, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+      });
+    } catch {
+      alert("멤버 초대 실패!");
+    }
   };
 
   return (
     <div>
-      
+      <h2>엔터 멤버 초대</h2>
+      <ul>
+        {entMemberList.map((member) => (
+          <li key={member.userId}>
+            <label>
+              <input
+                type="checkbox"
+                checked={selectedMembers.includes(member.userId)}
+                onChange={() => handleMemberSelection(member.userId)}
+              />
+              {member.nickname} ({member.email})
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button onClick={onClickInviteButton}>선택된 멤버 초대</button>
     </div>
   );
 };
