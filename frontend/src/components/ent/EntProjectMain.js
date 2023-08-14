71cd881b42129f119d2cf7ce9d0a42efd89737fb
nevermindperil <123xxx@kakao.com>
2023-08-14 14:13:03 +0900

    마이페이지 가입된 엔터목록

--- frontend/src/components/ent/EntProjectMain.js
+++ frontend/src/components/ent/EntProjectMain.js
@@ -91,24 +91,26 @@ const EntProjectMain = ({ userInfo }) => {
           <div className="studioBtn">스튜디오 입장</div>
         </Link>
       </div>
-      <h3>프로젝트 설명 : {project.projectInfo}</h3>
-
       <div>
-        <div className="pjtPartMember">
-          <span>파트</span>
-          <span>멤버</span>
+        <h3>프로젝트 설명 : {project.projectInfo}</h3>
+
+        <div className="pjtinfoWrap">
+          <div className="pjtPartMember">
+            <span>파트</span>
+            <span>멤버</span>
+          </div>
+          <span class="card__status">
+            {project.partList ? (
+              project.partList.map((part) => (
+                <div className="pjtPart_nickname">
+                  {part.partName} - {part.nickname}
+                </div>
+              ))
+            ) : (
+              <div>파트가 없습니다.</div>
+            )}
+          </span>
         </div>
-        <span class="card__status">
-          {project.partList ? (
-            project.partList.map((part) => (
-              <div className="pjtPart_nickname">
-                {part.partName} - {part.nickname}
-              </div>
-            ))
-          ) : (
-            <div>파트가 없습니다.</div>
-          )}
-        </span>
       </div>
       <div className="pjtClick">
         <div onClick={onClickDeleteProject}>프로젝트 삭제</div>
