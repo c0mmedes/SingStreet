1d6a8f215f515e1026061baa8adcbf3f80e89d93
강현곤 <shoot_1@naver.com>
2023-08-15 21:30:43 +0900

    http

--- frontend/src/components/user/MypageInvitedProject.js
+++ frontend/src/components/user/MypageInvitedProject.js
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 const MypageInvitedProject = ({ userInfo, myEntList }) => {
 	// useState
 	const [invitedProjectList, setInvitedProjectList] = useState([{}]);
-	const [isLoading, setIsLoading] = useState(true); // 로딩 상태
 	// axios 인스턴스 생성
 	const apiInstance = api();
 	// 페이지 이동을 위한 navigate 생성
@@ -25,10 +24,8 @@ const MypageInvitedProject = ({ userInfo, myEntList }) => {
 			});
 			console.log(res.data);
 			setInvitedProjectList(res.data);
-			setIsLoading(false); // 로딩 상태 변경
 		} catch {
 			alert("프로젝트 목록 가져오기 오류");
-			setIsLoading(false); // 에러 발생 시에도 로딩 상태 변경
 		}
 	};
 	// 수락 버튼 누르기
@@ -56,36 +53,32 @@ const MypageInvitedProject = ({ userInfo, myEntList }) => {
 			<div className="myEntListWrap">
 				<h1>초대 받은 프로젝트</h1>
 				{/* 초대장 불러오기 */}
-				{isLoading ? (
-					<div></div>
-				) : (
-					invitedProjectList
-						.filter((project) => project.isAccept !== null)
-						.map((project) => (
-							<div className="myEntListItemContainer">
-								<li className="myEntListItem" key={project.entId}>
-									<div className="myEntListItemTitleWrap">
-										<h3 className="myEntListItemTitle">엔터 이름 : {project.entName}</h3>
-										<h3 className="myEntListItemTitle">프로젝트 이름 : {project.projectName}</h3>
-										<button
-											onClick={() => {
-												onClickAccept(true, project.projectId, project.createdAt);
-											}}
-										>
-											수락
-										</button>
-										<button
-											onClick={() => {
-												onClickAccept(false, project.projectId, project.createdAt);
-											}}
-										>
-											거절
-										</button>
-									</div>
-								</li>
-							</div>
-						))
-				)}
+				{invitedProjectList
+					.filter((project) => project.isAccept !== null)
+					.map((project) => (
+						<div className="myEntListItemContainer">
+							<li className="myEntListItem" key={project.entId}>
+								<div className="myEntListItemTitleWrap">
+									<h3 className="myEntListItemTitle">엔터 이름 : {project.entName}</h3>
+									<h3 className="myEntListItemTitle">프로젝트 이름 : {project.projectName}</h3>
+									<button
+										onClick={() => {
+											onClickAccept(true, project.projectId, project.createdAt);
+										}}
+									>
+										수락
+									</button>
+									<button
+										onClick={() => {
+											onClickAccept(false, project.projectId, project.createdAt);
+										}}
+									>
+										거절
+									</button>
+								</div>
+							</li>
+						</div>
+					))}
 			</div>
 		</div>
 	);
