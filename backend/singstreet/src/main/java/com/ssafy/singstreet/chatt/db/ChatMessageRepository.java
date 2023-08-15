50914a9257df2b1d64ba3eb37db10840671551ff
LeeSW-k <swl9455@naver.com>
2023-08-15 18:31:47 +0900

    fix : chat

---
+++ backend/singstreet/src/main/java/com/ssafy/singstreet/chatt/db/ChatMessageRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.singstreet.chatt.db;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    Slice<ChatMessage> findAllByEntId(int entId, Pageable pageable);
+}
