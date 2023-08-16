c4b49c1c55194ca77a7b3cd9c0da7e00deafa06e
SongWonkyu <93499344+SongWonkyu@users.noreply.github.com>
2023-08-16 22:11:49 +0900

    프로젝트 상세페이지

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,24 +3,102 @@ import { Link } from "react-router-dom";
 import "../css/Home.css";
 import myImage from "../assets/sample1.png";
 import myImage2 from "../assets/sample2.png";
-import mainGif from "../assets/메인영상.gif";
+
 const Home = () => {
-  const backgroundStyle = {
-    backgroundImage: `url(${mainGif})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    height: "100vh", // 높이를 화면 높이로 설정
-    width: "100vw", // 너비를 화면 너비로 설정
-  };
   return (
-    <div style={backgroundStyle}>
+    <div>
       <Link to="/chat">
         <div>상우기의 채팅상담@@@@@@@@@@@@@@@@@@@@@@</div>
       </Link>
       <Link to="/studio">
         <div>studio</div>
       </Link>
+      <div className="mainHomeWrap">
+        <h1>메인영상 넣을거임</h1>
+      </div>
+      {/* <ul className="cards">
+        <li>
+          <a href="" class="card">
+            <img src={myImage} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <img
+                  class="card__thumb"
+                  src="https://i.imgur.com/7D7I6dI.png"
+                  alt=""
+                />
+                <div class="card__header-text">
+                  <h3 class="card__title">B110 엔터</h3>
+                  <span class="card__status">
+                    # KPOP # 뉴진스 # 10대 # 화이팅
+                  </span>
+                </div>
+              </div>
+              <p class="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={myImage2} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <img
+                  class="card__thumb"
+                  src="https://i.imgur.com/sjLMNDM.png"
+                  alt=""
+                />
+                <div class="card__header-text">
+                  <h3 class="card__title">kim Cattrall</h3>
+                  <span class="card__status">3 hours ago</span>
+                </div>
+              </div>
+              <p class="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img
+              src="https://i.imgur.com/2DhmtJ4.jpg"
+              class="card__image"
+              alt=""
+            />
+            <div class="card__overlay">
+              <div class="card__header">
+                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <img
+                  class="card__thumb"
+                  src="https://i.imgur.com/sjLMNDM.png"
+                  alt=""
+                />
+                <div class="card__header-text">
+                  <h3 class="card__title">kim Cattrall</h3>
+                  <span class="card__status">3 hours ago</span>
+                </div>
+              </div>
+              <p class="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+            </div>
+          </a>
+        </li>
+      </ul> */}
     </div>
   );
 };
