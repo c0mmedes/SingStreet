a287192092e07175eebb394438cf8bfaf02a527d
nevermindperil <123xxx@kakao.com>
2023-08-16 09:18:42 +0900

    엔터create

--- frontend/src/components/ent/EntCreate.js
+++ frontend/src/components/ent/EntCreate.js
@@ -3,154 +3,150 @@ import { useNavigate } from "react-router-dom";
 import { api } from "../../services/httpService";
 import "../../css/ent/EntCreate.css";
 const EntCreate = ({ userInfo, isLogin }) => {
-	const [entName, setEntName] = useState("");
-	const [entInfo, setEntInfo] = useState("");
-	const [entTagList, setEntTagList] = useState("");
-	const [isAutoAceepted, setIsAutoAceepted] = useState(true);
-	const [entImg, setEntImg] = useState(null);
-	const fileInputRef = useRef(null);
-	const handleEntName = (e) => {
-		setEntName(e.target.value);
-	};
-	const handleEntInfo = (e) => {
-		setEntInfo(e.target.value);
-	};
-	const handleEntTagList = (e) => {
-		setEntTagList(e.target.value);
-	};
-	const handleIsAutoAceepted = (e) => {
-		const value = e.target.value === "true"; // 문자열 "true"를 불리언 true로 변환
-		setIsAutoAceepted(value);
-	};
-	const handleImageDelete = () => {
-		setEntImg(null);
-		if (fileInputRef.current) {
-		  fileInputRef.current.value = "";
-		  fileInputRef.current.removeAttribute("required");
-		}
-	};
+  const [entName, setEntName] = useState("");
+  const [entInfo, setEntInfo] = useState("");
+  const [entTagList, setEntTagList] = useState("");
+  const [isAutoAceepted, setIsAutoAceepted] = useState(true);
+  const [entImg, setEntImg] = useState(null);
+  const fileInputRef = useRef(null);
+  const handleEntName = (e) => {
+    setEntName(e.target.value);
+  };
+  const handleEntInfo = (e) => {
+    setEntInfo(e.target.value);
+  };
+  const handleEntTagList = (e) => {
+    setEntTagList(e.target.value);
+  };
+  const handleIsAutoAceepted = (e) => {
+    const value = e.target.value === "true"; // 문자열 "true"를 불리언 true로 변환
+    setIsAutoAceepted(value);
+  };
+  const handleImageDelete = () => {
+    setEntImg(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+      fileInputRef.current.removeAttribute("required");
+    }
+  };
 
-	const handleEntImg = (e) => {
-		const file = e.target.files[0];
-		if (file) {
-		  const modifiedFile = new File([file], `${Date.now()}-${file.name}`, {
-			type: file.type,
-		  });
-		  setEntImg(modifiedFile);
-	  
-		  if (fileInputRef.current) {
-			fileInputRef.current.setAttribute("required", "required");
-		  }
-		} else {
-		  setEntImg(null);
-		  if (fileInputRef.current) {
-			fileInputRef.current.removeAttribute("required");
-		  }
-		}
-	};
+  const handleEntImg = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const modifiedFile = new File([file], `${Date.now()}-${file.name}`, {
+        type: file.type,
+      });
+      setEntImg(modifiedFile);
 
-	// axios 인스턴스 생성
-	const apiInstance = api();
-	// 페이지 이동을 위한 useNavigate를 사용하기 위한 변수 선언
-	const navigate = useNavigate();
+      if (fileInputRef.current) {
+        fileInputRef.current.setAttribute("required", "required");
+      }
+    } else {
+      setEntImg(null);
+      if (fileInputRef.current) {
+        fileInputRef.current.removeAttribute("required");
+      }
+    }
+  };
 
-	// 생성하기 클릭
-	const onClickEntCreate = async function (e) {
-		e.preventDefault(); // 기본 제출 동작 막기
+  // axios 인스턴스 생성
+  const apiInstance = api();
+  // 페이지 이동을 위한 useNavigate를 사용하기 위한 변수 선언
+  const navigate = useNavigate();
 
-		const accessToken = sessionStorage.getItem("accessToken");
-		// 엔터 프로필 이미지와, entData를 함께 보내기 위한 작업
-		const formData = new FormData();
-		formData.append("file", entImg);
-		const entData = {
-			entInfo: entInfo,
-			entName: entName,
-			entTagList: entTagList,
-			isAutoAccepted: isAutoAceepted,
-		};
-		formData.append(
-			"requestDto",
-			new Blob([JSON.stringify(entData)], {
-				type: "application/json",
-			})
-		);
+  // 생성하기 클릭
+  const onClickEntCreate = async function (e) {
+    e.preventDefault(); // 기본 제출 동작 막기
 
-		try {
-			const res = await apiInstance.post("/ent", formData, {
-				headers: {
-					Authorization: `Bearer ${accessToken}`, // Bearer 토큰 포함
-					"Content-Type": "multipart/form-data",
-					Accept: "application/json", // 추가
-				},
-			});
-			console.log(res);
-			if (res.data === false) {
-				alert("중복된 엔터명입니다!");
-			} else {
-				alert("엔터 생성 완료");
-				navigate("/ent");
-			}
-		} catch (error) {
-			alert("엔터 생성 오류");
-		}
-	};
-	return (
-		<div>
-			<div className="form_wrapper entCreateFormWrapper">
-				<div className="form_container">
-					<div className="title_container">
-						<h2>엔터 신규 등록</h2>
-					</div>
+    const accessToken = sessionStorage.getItem("accessToken");
+    // 엔터 프로필 이미지와, entData를 함께 보내기 위한 작업
+    const formData = new FormData();
+    formData.append("file", entImg);
+    const entData = {
+      entInfo: entInfo,
+      entName: entName,
+      entTagList: entTagList,
+      isAutoAccepted: isAutoAceepted,
+    };
+    formData.append(
+      "requestDto",
+      new Blob([JSON.stringify(entData)], {
+        type: "application/json",
+      })
+    );
 
-					<div className="row clearfix">
-						<div className="">
-							<form className="entCreateForm" onSubmit={onClickEntCreate}>
-								<label>엔터명</label>
-								<div className="input_field">
-									<input
-										type="text"
-										maxlength="20"
-										name="entId"
-										value={entName}
-										onChange={handleEntName}
-										required
-									/>
-								</div>
-								<div className="input_field">
-									{/* <input type="radio" id="ex_rd" name="ex_rds"> 
-                  <label for="ex_rd">라디오버튼</label> */}
-								</div>
-								<label>엔터 소개</label>
-								<div className="input_field">
-									<textarea
-										id="entInfo"
-										type="text"
-										name="entInfo"
-										value={entInfo}
-										onChange={handleEntInfo}
-										required
-									/>
-								</div>
-								<label>해시태그</label>
-								<div className="input_field">
-									<input
-										type="text"
-										name="tagList"
-										placeholder="#뉴진스 #하입보이"
-										value={entTagList}
-										onChange={handleEntTagList}
-										required
-									/>
-								</div>
-								<label>공개여부</label>
-								<div className="input_field select_option">
-									<select onChange={handleIsAutoAceepted}>
-										<option value="true">공개엔터 (바로 가입)</option>
-										<option value="false">공개엔터 (승인 대기)</option>
-									</select>
-									<div className="select_arrow"></div>
-								</div>
-								<label>엔터 프로필</label>
+    try {
+      const res = await apiInstance.post("/ent", formData, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`, // Bearer 토큰 포함
+          "Content-Type": "multipart/form-data",
+          Accept: "application/json", // 추가
+        },
+      });
+      console.log(res);
+      if (res.data === false) {
+        alert("중복된 엔터명입니다!");
+      } else {
+        alert("엔터 생성 완료");
+        navigate("/ent");
+      }
+    } catch (error) {
+      alert("엔터 생성 오류");
+    }
+  };
+  return (
+    <div>
+      <div className="form_wrapper entCreateFormWrapper">
+        <div className="form_container">
+          <div className="title_container">
+            <h2>엔터 신규 등록</h2>
+          </div>
+
+          <div className="row clearfix">
+            <div className="">
+              <form className="entCreateForm" onSubmit={onClickEntCreate}>
+                <label>엔터명</label>
+                <div className="input_field">
+                  <input
+                    type="text"
+                    maxlength="20"
+                    name="entId"
+                    value={entName}
+                    onChange={handleEntName}
+                    required
+                  />
+                </div>
+                <label>엔터 소개</label>
+                <div className="input_field">
+                  <textarea
+                    id="entInfo"
+                    type="text"
+                    name="entInfo"
+                    value={entInfo}
+                    onChange={handleEntInfo}
+                    required
+                  />
+                </div>
+                <label>해시태그</label>
+                <div className="input_field">
+                  <input
+                    type="text"
+                    name="tagList"
+                    placeholder="#뉴진스 #하입보이"
+                    value={entTagList}
+                    onChange={handleEntTagList}
+                    required
+                  />
+                </div>
+                <label>공개여부</label>
+                <div className="input_field select_option">
+                  <select onChange={handleIsAutoAceepted}>
+                    <option value="true">공개엔터 (바로 가입)</option>
+                    <option value="false">공개엔터 (승인 대기)</option>
+                  </select>
+                  <div className="select_arrow"></div>
+                </div>
+                <label>엔터 프로필</label>
                 <div className="input_field" id="entImgInputField">
                   <div className="file-input">
                     <input
@@ -159,9 +155,11 @@ const EntCreate = ({ userInfo, isLogin }) => {
                       ref={fileInputRef}
                       onChange={handleEntImg}
                       accept="image/*"
-                      style={{ display: 'none' }}
+                      style={{ display: "none" }}
                     />
-                    <span id="modifybutton" onClick={() => fileInputRef.current.click()}>
+                    <span
+                      id="modifybutton"
+                      onClick={() => fileInputRef.current.click()}>
                       이미지 변경
                     </span>
                     <span id="deletebutton" onClick={handleImageDelete}>
