850fbd3e28b39d282eccbaa55f8b1797269017be
LeeSW-k <swl9455@naver.com>
2023-08-16 12:23:11 +0900

    chore: chatting test

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
 function api() {
-<<<<<<< HEAD
-  const instance = axios.create({
-    // baseURL: "https://i9b110.p.ssafy.io/backend", // API 서버의 기본 URL
-    baseURL: "http://localhost:8080", // API 서버의 기본 URL
-=======
+
 	const instance = axios.create({
 		baseURL: "https://i9b110.p.ssafy.io/backend", // API 서버의 기본 URL
->>>>>>> f4f0c701245387f01b98dbc0eb1d0e413158b0f5
-
 		timeout: 5000, // 요청 시간 초과 설정 (옵션)
 		headers: {
 			"Content-Type": "application/json",
