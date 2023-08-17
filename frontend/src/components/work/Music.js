af41cb481ffdf7f6800bd9e67f3e6875684b2cbc
강현곤 <shoot_1@naver.com>
2023-08-18 02:58:01 +0900

    fix: ㅇㅇ

--- frontend/src/components/work/Music.js
+++ frontend/src/components/work/Music.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState, useRef } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { api } from "../../services/httpService";
 import "../../css/work/Music.css";
+import MusicDetail from "./MusicDetail"; // 모달 컴포넌트 임포트
 
 const Music = () => {
 	const [musicList, setMusicList] = useState([]);
+	const [isModalOpen, setIsModalOpen] = useState(false); // 모달 열림 여부 상태
 	// axios 인스턴스 생성
 	const apiInstance = api();
 	// 페이지 이동을 위한 useNavigate를 사용하기 위한 변수 선언
@@ -13,6 +15,11 @@ const Music = () => {
 	useEffect(() => {
 		getInitialMusicList();
 	}, []);
+
+	// 모달 토글 함수
+	const toggleModal = () => {
+		setIsModalOpen(!isModalOpen);
+	};
 
 	const getInitialMusicList = async () => {
 		const res = await apiInstance.get(`/project/music`);
@@ -33,7 +40,7 @@ const Music = () => {
 			</form>
 
 			<div class="cards-list">
-				{/* 뮤직리스트 */}
+				{/* //뮤직리스트 라우터형식으로 구현
 				{musicList.map((music) => (
 					<Link to={`/music/${music.projectId}`}>
 						<div class="musicCard 1">
@@ -48,7 +55,24 @@ const Music = () => {
 							</div>
 						</div>
 					</Link>
+        ))} */}
+
+				{musicList.map((music) => (
+					<div class="musicCard 1" onClick={toggleModal}>
+						<div class="card_image">
+							<img src={music.projectImg} />
+						</div>
+						<div class="card_title title-white music">
+							<p>{music.projectName}</p>
+							<p>
+								{music.singName} - {music.singerName}
+							</p>
+						</div>
+					</div>
 				))}
+
+				{/* 모달 */}
+				<MusicDetail isOpen={isModalOpen} onClose={toggleModal} />
 
 				{
 					// 더미 데이터
