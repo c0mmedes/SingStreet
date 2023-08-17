c18209502957abf2049b4af33d02f253baa69f3d
강현곤 <shoot_1@naver.com>
2023-08-18 04:47:16 +0900

    Feat:entFeed에 채팅붙이기

--- frontend/src/containers/chat/ChatContainer.js
+++ frontend/src/containers/chat/ChatContainer.js
@@ -4,25 +4,20 @@ import { addUserInfo, setIsLogin } from "../../modules/user/user";
 import Chat from "../../components/chat/Chat";
 
 const ChatContainer = ({ userInfo, isLogin, setIsLogin }) => {
-  return (
-    <Chat
-      userInfo={userInfo}
-      isLogin={isLogin}
-      setIsLogin={setIsLogin}></Chat>
-  );
+	return <Chat userInfo={userInfo} isLogin={isLogin} setIsLogin={setIsLogin}></Chat>;
 };
 
 const mapStateToProps = (state) => ({
-  userInfo: state.user.userInfo,
-  isLogin: state.user.isLogin,
+	userInfo: state.user.userInfo,
+	isLogin: state.user.isLogin,
 });
 const mapDispatchToProps = (dispatch) => ({
-  addUserInfo: (userInfo) => {
-    dispatch(addUserInfo(userInfo));
-  },
-  setIsLogin: () => {
-    dispatch(setIsLogin());
-  },
+	addUserInfo: (userInfo) => {
+		dispatch(addUserInfo(userInfo));
+	},
+	setIsLogin: () => {
+		dispatch(setIsLogin());
+	},
 });
 
 // Login 컴포넌트에선 dispatch만 사용하고 리덕스 '스토어의 상태'를 사용하지않음
