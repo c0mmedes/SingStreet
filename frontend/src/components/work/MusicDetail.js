32d1052b1ae0ab8e5aad8dd08cef9f449a970fdb
강현곤 <shoot_1@naver.com>
2023-08-18 03:04:16 +0900

    fix: MusicDetail 모달

--- frontend/src/components/work/MusicDetail.js
+++ frontend/src/components/work/MusicDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { api } from "../../services/httpService";
+import "../../css/work/";
 
 // const MusicDetail = () => {
 // 	// axios 인스턴스
