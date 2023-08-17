6a0f490311c79df532dee594ef93cd28143b386d
강현곤 <shoot_1@naver.com>
2023-08-18 03:11:07 +0900

    fix: MusicDetail css 닫기버튼 오른쪽위로

--- frontend/src/components/work/MusicDetail.js
+++ frontend/src/components/work/MusicDetail.js
@@ -3,16 +3,17 @@ import { useParams, useNavigate } from "react-router-dom";
 import { api } from "../../services/httpService";
 import "../../css/work/MusicDetail.css";
 
-// const MusicDetail = () => {
-// 	// axios 인스턴스
-// 	const apiInstance = api();
-// 	// 라우터 파라미터에서 가져올 entId 변수
-// 	const { projectId } = useParams();
+/* //라우터 방식으로 구현했을때 쓰려했던거
+const MusicDetail = () => {
+	// axios 인스턴스
+	const apiInstance = api();
+	// 라우터 파라미터에서 가져올 entId 변수
+	const { projectId } = useParams();
 
-// 	return <div></div>;
-// };
+	return <div></div>;
 
-// export default MusicDetail;
+    export default MusicDetail;
+}; */
 
 const MusicDetail = ({ isOpen, onClose }) => {
 	if (!isOpen) {
@@ -21,7 +22,9 @@ const MusicDetail = ({ isOpen, onClose }) => {
 
 	return (
 		<div className="modal">
-			<button onClick={onClose}>닫기</button>
+			<button className="close-button" onClick={onClose}>
+				닫기
+			</button>
 			<div className="modal-content">
 				{/* 모달 내용 */}
 				<h2>모달 창 내용</h2>
